7ac21c051ab3488e58d0293af57300992127c917
Robert <robert.p1991@gmail.com>
2017-11-11 23:59:05 +0100

    make selfies

--- app/containers/Camera/index.js
+++ app/containers/Camera/index.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import {Text, StyleSheet} from 'react-native'
+import {Text, StyleSheet, View} from 'react-native'
 import Camera from 'react-native-camera'
 import RNFS from 'react-native-fs'
 import ImageResizerService from '../../../Services/ImageResizer.service'
@@ -10,6 +10,20 @@ export default class CameraComponent extends Component{
       title: 'Camera'
     }
 
+    static CAMERA_OPTIONS = {
+      audio: 'false', // IOS only
+      mode: 'still',
+      target: 'temp',
+      jpegQuality: '70'
+    }
+
+    constructor(props, context) {
+      super(props, context)
+      this.state = {
+        emotionValues: {}
+      }
+    }
+
     render() {
         return (
             <Camera
@@ -17,15 +31,24 @@ export default class CameraComponent extends Component{
               this.camera = cam;
             }}
             style={styles.preview}
-            aspect={Camera.constants.Aspect.fill}>
+            aspect={Camera.constants.Aspect.fill}
+            type={Camera.constants.Type.front}>
+
+            { Object.keys(this.state.emotionValues)
+              .map((emotion, i) => {
+                return <View key={ 'emotion:' + i }>
+                          <Text style={styles.valuesPreview} key={ 'key:' + i }>{ emotion }</Text>
+                          <Text style={styles.valuesPreview} key={ 'value:' + i }>{ this.state.emotionValues[emotion] }</Text>
+                       </View>
+              }) }
+
             <Text style={styles.capture} onPress={this.takePicture.bind(this)}>MACH FOTO!</Text>
           </Camera>
         )
     }
 
     takePicture() {
-        const options = {};
-        this.camera.capture({metadata: options})
+        this.camera.capture({metadata: this.CAMERA_OPTIONS})
         .then(data => ImageResizerService.resizeImage(data.path))
         .then(resizedImageUri => this._getBlobFromImagePath(resizedImageUri))
         .then(base64image => this._getEmotionsFromImage(base64image))
@@ -40,7 +63,9 @@ export default class CameraComponent extends Component{
 
     _getEmotionsFromImage(base64image) {
       APIUtils.getEmotions(base64image)
-      .then(res => console.log(res))
+      .then(emotions => {
+        this.setState((state) => state.emotionValues = emotions[0].scores)
+      })
     } 
 }
 
@@ -57,5 +82,8 @@ const styles = StyleSheet.create({
       color: '#000',
       padding: 10,
       margin: 40
+    },
+    valuesPreview: {
+      color: 'white'
     }
   });
