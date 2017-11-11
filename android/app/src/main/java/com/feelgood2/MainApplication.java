9c93a707c9849dcdc50408bfe2a325d387d1e496
Robert <robert.p1991@gmail.com>
2017-11-11 20:23:20 +0100

    add image resizer

--- android/app/src/main/java/com/feelgood2/MainApplication.java
+++ android/app/src/main/java/com/feelgood2/MainApplication.java
@@ -3,6 +3,9 @@ package com.feelgood2;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import fr.bamlab.rnimageresizer.ImageResizerPackage;
+import com.rnfs.RNFSPackage;
+import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +25,10 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new ImageResizerPackage(),
+            new RNFSPackage(),
+            new RCTCameraPackage()
       );
     }
   };
