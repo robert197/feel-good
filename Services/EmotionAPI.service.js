4f2ebf092cfdda861ebe6d928051b016151f7551
Robert <robert.p1991@gmail.com>
2017-12-05 21:11:27 +0100

    update emotion api key

--- Services/EmotionAPI.service.js
+++ Services/EmotionAPI.service.js
@@ -3,14 +3,13 @@ import RNFetchBlob from 'react-native-fetch-blob'
 const APIUtils = {
     getEmotions(base64image) {
         const endpoint = "https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize?"
-        const key1 = "6ead7712d719430099113c03f259fc22"
-        const key2 = "84d2f19028694aa8b8d5ec69ccd7c69a"
+        const key = "98cd8c2f327e4d90b8720404efc90625"
         return RNFetchBlob.fetch(
             'POST',
             endpoint,
             {
                 'Content-Type': 'application/octet-stream',
-                'Ocp-Apim-Subscription-Key': key1
+                'Ocp-Apim-Subscription-Key': key
             },
             base64image
         )
